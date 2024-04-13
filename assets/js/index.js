de2a324332fbd1c20564bd0f1b48ae69bfde2ee1
Daryl Bacurin <darylbacurin14@gmail.com>
2024-04-13 21:39:08 +0800

    Latest version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,29 @@
 const faqQaContainers = document.querySelectorAll(".faq-qa-container");
 
 faqQaContainers.forEach(faqQaContainer => {
-    faqQaContainer.addEventListener('click', () => {
-        const faqIconImg = faqQaContainer.querySelector('img.faq-qa-icon');
-        const faqAnswerParagraph = faqQaContainer.querySelector('p.faq-qa-answer');
+    const faqIconImg = faqQaContainer.querySelector('.faq-qa-icon');
+    const faqAnswerParagraph = faqQaContainer.querySelector('.faq-qa-answer');
+    const faqQuestion = faqQaContainer.querySelector('.faq-qa-icon-container');
 
+    faqQuestion.addEventListener('click', () => {
+        // If the clicked section is already open, do nothing
+        if (faqAnswerParagraph.classList.contains('show-answer')) {
+            return;
+        }
+
+        // Close all other accordion items
+        faqQaContainers.forEach(container => {
+            if (container !== faqQaContainer) {
+                const icon = container.querySelector('.faq-qa-icon');
+                const answer = container.querySelector('.faq-qa-answer');
+
+                icon.src = './assets/images/icon-plus.svg';
+                icon.alt = 'Faqs icon plus';
+                answer.classList.remove('show-answer');
+            }
+        });
+
+        // Toggle current accordion item
         faqIconImg.src = faqIconImg.src.includes('icon-plus.svg')
             ? './assets/images/icon-minus.svg'
             : './assets/images/icon-plus.svg';
@@ -13,6 +32,5 @@ faqQaContainers.forEach(faqQaContainer => {
             : 'Faqs icon plus';
 
         faqAnswerParagraph.classList.toggle('show-answer');
-        console.log(faqAnswerParagraph);
     });
 });
